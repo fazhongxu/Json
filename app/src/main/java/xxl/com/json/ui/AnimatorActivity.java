147a7da0b97c8ba9ab9906bf3f4128c68c488851
fazhongxu <fazhongxu0914@163.com>
2017-12-03 16:48:30 +0800

    动画性能优化

--- app/src/main/java/xxl/com/json/ui/AnimatorActivity.java
+++ app/src/main/java/xxl/com/json/ui/AnimatorActivity.java
@@ -2,8 +2,10 @@ package xxl.com.json.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import xxl.com.json.R;
+import xxl.com.json.view.LoadingView;
 
 public class AnimatorActivity extends BaseActivity {
 
@@ -11,5 +13,12 @@ public class AnimatorActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animator);
+        final LoadingView loadingView = (LoadingView) findViewById(R.id.loading_view);
+        loadingView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadingView.setVisibility(View.GONE);
+            }
+        });
     }
 }
--- app/src/main/java/xxl/com/json/view/LoadingView.java
+++ app/src/main/java/xxl/com/json/view/LoadingView.java
@@ -9,6 +9,7 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.LinearLayout;
@@ -24,6 +25,7 @@ public class LoadingView extends LinearLayout {
     private View mShadowView;
     private int ANIMATOR_DURATION = 500;
     private float mTranslationY;
+    private boolean mIsStopAnimation = false;
 
     public LoadingView(Context context) {
         this(context, null);
@@ -37,7 +39,12 @@ public class LoadingView extends LinearLayout {
         super(context, attrs, defStyleAttr);
         initLayout();
         //开始下落动画
-        startFallAnimator();
+        post(new Runnable() {//onResume View绘制流程执行完毕之后调用
+            @Override
+            public void run() {
+                startFallAnimator();
+            }
+        });
     }
 
     /**
@@ -56,15 +63,18 @@ public class LoadingView extends LinearLayout {
      * 开始下落动画
      */
     private void startFallAnimator() {
+        if (mIsStopAnimation){
+            return;
+        }
         //属性动画 下落  越来越快（差值器）
         //形状 下落 的同时 阴影由大变小
         //mShadowView.setTranslationY();
         //mShapeView.setAlpha();
         //mShapeView.setScaleY();
-        ObjectAnimator translationAnimator = ObjectAnimator.ofFloat(mShapeView,"translationY",0,mTranslationY);
+        ObjectAnimator translationAnimator = ObjectAnimator.ofFloat(mShapeView, "translationY", 0, mTranslationY);
         //translationAnimator.setDuration();
         //translationAnimator.start();
-        ObjectAnimator scaleX = ObjectAnimator.ofFloat(mShadowView,"scaleX",1,0.3F);
+        ObjectAnimator scaleX = ObjectAnimator.ofFloat(mShadowView, "scaleX", 1, 0.3F);
 
         //动画集
         AnimatorSet animatorSet = new AnimatorSet();
@@ -72,7 +82,7 @@ public class LoadingView extends LinearLayout {
         //设置动画差值器，下落的时候是越来越快，上抛是越来越慢
         animatorSet.setInterpolator(new AccelerateInterpolator());
         //一起执行
-        animatorSet.playTogether(translationAnimator,scaleX);
+        animatorSet.playTogether(translationAnimator, scaleX);
 
         animatorSet.addListener(new AnimatorListenerAdapter() {
 
@@ -90,13 +100,16 @@ public class LoadingView extends LinearLayout {
      * 开始上抛动画
      */
     private void startUpAnimator() {
+        if (mIsStopAnimation){
+            return;
+        }
         ObjectAnimator translationAnimator = ObjectAnimator.ofFloat(mShapeView, "translationY", mTranslationY, 0);
-        ObjectAnimator scaleAnimator = ObjectAnimator.ofFloat(mShadowView, "scaleX", 0.3F,1);
+        ObjectAnimator scaleAnimator = ObjectAnimator.ofFloat(mShadowView, "scaleX", 0.3F, 1);
 
         AnimatorSet animatorSet = new AnimatorSet();
         animatorSet.setDuration(ANIMATOR_DURATION);
         animatorSet.setInterpolator(new DecelerateInterpolator());
-        animatorSet.playTogether(translationAnimator,scaleAnimator);
+        animatorSet.playTogether(translationAnimator, scaleAnimator);
         animatorSet.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
@@ -119,22 +132,37 @@ public class LoadingView extends LinearLayout {
      * 开始旋转
      */
     private void startRotateAnimation() {
-        //获取当前形状 旋转  圆形不用旋转，正方形旋转180 三角形旋转60度
+        //获取当前形状 旋转  圆形不用旋转，正方形旋转180 三角形旋转120度
         ObjectAnimator scaleAnimator = null;
         switch (mShapeView.getCurrentShape()) {
             case CIRCLE:
             case SQUARE:
-                scaleAnimator = ObjectAnimator.ofFloat(mShapeView,"rotation",0,180);
+                scaleAnimator = ObjectAnimator.ofFloat(mShapeView, "rotation", 0, 180);
                 break;
             case TRIANGLE:
-                scaleAnimator = ObjectAnimator.ofFloat(mShapeView,"rotation",0,120);
+                scaleAnimator = ObjectAnimator.ofFloat(mShapeView, "rotation", 0, 120);
                 break;
         }
         scaleAnimator.setDuration(ANIMATOR_DURATION);
         scaleAnimator.start();
     }
 
+    @Override
+    public void setVisibility(int visibility) {
+        super.setVisibility(View.INVISIBLE);//INVISIBLE 不重新计算和摆放，性能优化
+        mShapeView.clearAnimation();//清理动画
+        mShadowView.clearAnimation();
+
+        //把LoadingView从其parent中移除
+        ViewGroup parent = (ViewGroup) getParent();
+        if (null != parent) {
+            parent.removeView(this);
+        }
+
+        mIsStopAnimation = true;
+    }
+
     private float dip2px(int dip) {
-        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,dip,getResources().getDisplayMetrics());
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dip, getResources().getDisplayMetrics());
     }
 }

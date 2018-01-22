caf84938e3f1b2850d08b8f13bb23bd2b4f4275a
fazhongxu <fazhongxu0914@163.com>
2018-01-22 19:59:37 +0800

    Jsoup init

---
+++ app/src/main/java/xxl/com/json/bean/Girl.java
@@ -0,0 +1,53 @@
+package xxl.com.json.bean;
+
+/**
+ * Created by xxl on 2018/1/22.
+ */
+
+public class Girl {
+    private String title;
+    private String imagUrl;
+    private String imgWidth;
+    private String imgHeight;
+    private String groupId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImagUrl() {
+        return imagUrl;
+    }
+
+    public void setImagUrl(String imagUrl) {
+        this.imagUrl = imagUrl;
+    }
+
+    public String getImgWidth() {
+        return imgWidth;
+    }
+
+    public void setImgWidth(String imgWidth) {
+        this.imgWidth = imgWidth;
+    }
+
+    public String getImgHeight() {
+        return imgHeight;
+    }
+
+    public void setImgHeight(String imgHeight) {
+        this.imgHeight = imgHeight;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+}
---
+++ app/src/main/java/xxl/com/json/ui/GirlActivity.java
@@ -0,0 +1,29 @@
+package xxl.com.json.ui;
+
+import xxl.com.json.R;
+import xxl.com.json.mvp.demo4.base.BaseMvpActivity;
+import xxl.com.json.mvp.demo4.base.BasePresenter;
+
+public class GirlActivity extends BaseMvpActivity {
+
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected BasePresenter createPresenter() {
+        return null;
+    }
+
+    @Override
+    protected void setContentView() {
+        setContentView(R.layout.activity_girl);
+    }
+}

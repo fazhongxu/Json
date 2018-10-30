28233aa0ca6e33ec0ffd7ba12ee3ec8ca98e46ad
fazhongxu <fazhongxu0914@163.com>
2018-10-30 18:55:59 +0800

    add ButterKnife to module_web

--- module_web/src/main/java/com/xxl/modelweb/SimpleWebViewActivity.java
+++ module_web/src/main/java/com/xxl/modelweb/SimpleWebViewActivity.java
@@ -7,10 +7,14 @@ import com.alibaba.android.arouter.facade.annotation.Route;
 import com.xxl.mediatorweb.IConstantWeb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 @Route(path = IConstantWeb.WEB_SIMPLE_WEB)
 public class SimpleWebViewActivity extends AppCompatActivity {
 
+    @BindView(R2.id.web_refresh_rfwv)
+    RefreshWebView webRefreshRfwv;
     private RefreshWebView mRefreshWebView;
     private String mUrl;
 
@@ -22,6 +26,7 @@ public class SimpleWebViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_web_view);
+        ButterKnife.bind(this);
         mRefreshWebView = findViewById(R.id.web_refresh_rfwv);
         SimpleWebView webView = mRefreshWebView.getWebView();
         mUrl = getIntent().getStringExtra(IConstantWeb.URL);

37d8b70734c53aaf6986bad6b2af79eaa6760f58
fazhongxu <fazhongxu0914@163.com>
2018-11-12 19:28:23 +0800

    add media module

---
+++ module_media/src/main/java/com/xxl/module/media/AudioCapture.java
@@ -0,0 +1,65 @@
+package com.xxl.module.media;
+
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by xxl on 2018/11/12.
+ *
+ * Description :
+ */
+
+public class AudioCapture {
+    private static final int DEFAULT_SOURCE = MediaRecorder.AudioSource.MIC;
+    private static final int DEFAULT_SAMPLE_RATE = 44100;
+    private static final int DEFAULT_AUDIO_FORMAT = MediaRecorder.OutputFormat.DEFAULT;
+    private static final int DEFAULT_AUDIO_ENCODER = MediaRecorder.AudioEncoder.DEFAULT;
+    private MediaRecorder mMediaRecorder;
+
+    public AudioCapture() {
+        if (mMediaRecorder == null) {
+            recordeConfig();
+        }
+    }
+
+    private void recordeConfig() {
+        mMediaRecorder = new MediaRecorder();
+        mMediaRecorder.setOutputFile(Environment.getExternalStorageDirectory().getAbsolutePath()+File.separator+"test.wav");
+        mMediaRecorder.setAudioSource(DEFAULT_SOURCE);
+        mMediaRecorder.setAudioSamplingRate(DEFAULT_SAMPLE_RATE);
+        mMediaRecorder.setOutputFormat(DEFAULT_AUDIO_FORMAT);
+        mMediaRecorder.setAudioEncoder(DEFAULT_AUDIO_ENCODER);
+        mMediaRecorder.setOnInfoListener(new MediaRecorder.OnInfoListener() {
+            @Override
+            public void onInfo(MediaRecorder mr, int what, int extra) {
+                Log.e("aaaa", "onInfo: " );
+            }
+        });
+    }
+
+
+    public void startCapture() {
+        if (mMediaRecorder == null) {
+            recordeConfig();
+        }
+        try {
+            mMediaRecorder.prepare();
+            mMediaRecorder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void stopCapture() {
+        if (mMediaRecorder != null) {
+            mMediaRecorder.stop();
+            mMediaRecorder.reset();
+            mMediaRecorder.release();
+            mMediaRecorder = null;
+        }
+    }
+}
---
+++ module_media/src/main/java/com/xxl/module/media/MediaActivity.java
@@ -0,0 +1,49 @@
+package com.xxl.module.media;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class MediaActivity extends AppCompatActivity {
+
+    private AudioCapture mAudioCapture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R2.id.activity_media_start_btn, R2.id.activity_media_stop_btn})
+    public void onViewClicked(View view) {
+        int id = view.getId();
+        if (id == R.id.activity_media_start_btn) {
+            RxPermissions rxPermissions = new RxPermissions(this);
+            Disposable subscribe = rxPermissions.request(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.RECORD_AUDIO)
+                    .observeOn(Schedulers.io())
+                    .subscribe(aBoolean -> {
+                        if (aBoolean) {
+                            mAudioCapture = new AudioCapture();
+                            mAudioCapture.startCapture();
+                        } else {
+                            Toast.makeText(this, "请开启录制权限！", Toast.LENGTH_SHORT).show();
+                        }
+
+                    });
+        }else if (id == R.id.activity_media_stop_btn) {
+            mAudioCapture.stopCapture();
+        }
+    }
+}
